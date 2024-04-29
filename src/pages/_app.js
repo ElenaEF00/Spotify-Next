cbd1349c3ee92748a44f4cba6b1068cb93eea7e1
Elena Faraci <elena.faraci60@gmail.com>
2024-04-29 16:24:15 +0200

    fixing error in deployment

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,9 @@
-import { Main } from "next/document";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-        <Component {...pageProps} />
+      <Component {...pageProps} />
     </>
   );
 }
